d951af22f076e00d7071f683f78a2225cdda7d08
Ahmad Syaifudin <sysfdn@gmail.com>
2020-03-08 20:14:58 +0700

    Update hitung_luas_bidang.py

--- python/hitung_luas_bidang.py
+++ python/hitung_luas_bidang.py
@@ -1,4 +1,12 @@
+"""
+    Nama: Ahmad Syaifudin
+    NIM: 192410103093
+    Program: Perhitungan luas suatu bangun.
+    Source: https://github.com/sysfdn/baris-kode/edit/master/python/hitung_luas_bidang.py
+"""
+
 # Preparation
+# nama_variabel: tipe data = nilai
 choose: str = ""
 luas: float = 0
 panjang: float = 0
@@ -6,35 +14,55 @@ lebar: float = 0
 alas: float = 0
 tinggi: float = 0
 PHI: float = 3.14
+jari_jari: float = 0
 is_done: bool = False
-while is_done == False:
-    print("=== Program Penghitung Luas === ")
-    print("Daftar Bidang: ")
-    print("1> Segi Empat")
-    print("2> Segi Tiga")
-    print("3> Lingkaran")
-    print("0> Keluar")
+
+# Ketika nilai variable is_done adalah False maka program akan berjalan, jika tidak maka akan berhenti.
+while is_done is not True:
+    # Menampilkan daftar menu pilihan
+    print("=== Program Penghitung Luas === "
+    	"\nDaftar Bidang: ",
+    	"\n1> Segiempat",
+    	"\n2> Segitiga",
+    	"\n3> Lingkaran",
+    	"\n0> Keluar"
+    )
+    # User menginputkan pilihannya, dalam bentuk str
     choose = input("Pilih Menu> ")
     if choose == "1":
-        print("Menghitung Luas Segi Empat")
-        panjang = float(input("Input Panjang (cm): "))
-        lebar = float(input("Input Lebar (cm): "))
+        # Ketika user memilih nomor 1, maka program akan menampilkan menu menghitung luas Segiempat.
+        print("Menghitung Luas Segiempat")
+        # Program meminta user untuk menginputkan panjang dan lebar sisi bangun.
+        panjang = float(input("Input nilai sisi panjang (cm): "))
+        lebar = float(input("Input nilai sisi lebar (cm): "))
+        # Program memproses perhitungan
         luas = panjang * lebar
-        print("Hasil Luas Segi Empat: ", luas)
+        # Program Menampilkan hasil hitung
+        print("Hasil hitung luas Segiempat: ", luas)
     elif choose == "2":
-        print("Menghitung Luas Segi Tiga")
-        alas = float(input("Input Alas (cm): "))
-        tinggi = float(input("Input Tinggi (cm): "))
+        # Ketika user memilih nomor 2, maka program akan menampilkan menu menghitung luas segitiga.
+        print("Menghitung Luas Segitiga")
+        # Program meminta user untuk menginputkan alas dan tinggi sisi bangun.
+        alas = float(input("Input nilai sisi alas (cm): "))
+        tinggi = float(input("Input nilai sisi tinggi (cm): "))
+        # Program memproses perhitungan
         luas = 0.5 * alas * tinggi
-        print("Hasil Luas Segi Tiga: ", luas)
+        # Program Menampilkan hasil hitung
+        print("Hasil hitung luas segitiga: ", luas)
     elif choose == "3":
+        # Ketika user memilih nomor 3, maka program akan menampilkan menu menghitung luas lingkaran.
         print("Menghitung Luas Lingkaran")
-        jari_jari = float(input("Input Jari-jari (cm): "))
+        # Program meminta user untuk menginputkan jari-jari bangun.
+        jari_jari = float(input("Input nilai jari-jari (cm): "))
+        # Program memproses perhitungan
         luas = PHI * jari_jari**2
-        print("Hasil Luas Lingkaran: ", luas)
+        # Program Menampilkan hasil hitung
+        print("Hasil hitung luas Lingkaran: ", luas)
     elif choose == "0":
-        print("Program berhenti, Terimakasih!")
+        # Ketika user memilih nomor o, maka program akan mengubah nilai variabel `is_done` menjadi True.
+        print("Program berhenti, Bye!")
         is_done = True
     else:
-        print("Kamu memilih bidang yang salah")
+        # Ketika user memilih menu/opsi yang salah maka program akan mentrigger user
+        print("Kamu memilih Menu yang salah!")
     print()
