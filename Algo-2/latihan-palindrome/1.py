4609dd8559602450ff7aea65b2502ad7d106a146
Ahmad Syaifudin <sysfdn@gmail.com>
2021-03-06 07:23:06 +0700

    Update and rename Algo-2/latihan-palindrome/1.py to Algo-2/latihan_palindrome/1.py

--- Algo-2/latihan-palindrome/1.py
+++
@@ -1,7 +0,0 @@
-# is_palindrome, mengecek string apakah palindrome atau bukan
-def is_palindrome(string):
-    reverse_str = string[::-1]
-    if string == reverse_str:
-        return True
-    else:
-        return False
